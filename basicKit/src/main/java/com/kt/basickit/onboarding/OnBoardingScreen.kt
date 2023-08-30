7be7b9f81035d7eb52cce408197ac080f99de5fd
lena-for-world <kimrlyunah@gmail.com>
2023-08-30 12:00:59 +0900

    [feat] README 및 주석 추가

--- basicKit/src/main/java/com/kt/basickit/onboarding/OnBoardingScreen.kt
+++ basicKit/src/main/java/com/kt/basickit/onboarding/OnBoardingScreen.kt
@@ -8,10 +8,18 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 
+/**
+ * OnBoarding 스탭을 출력 하는 컴포저블.
+ *
+ * @param steps 사용자가 정의한 OnBoarding 스탭 리스트
+ * @param startStep 시작 스탭 인덱스. 기본 값으로 0 사용.
+ * @param onBoardingTopArea O스nBoarding 각 스탭 화면에 관계 없이 유지할 상단 영역 컴포저블. 없으면 출력 하지 않는다.
+ * @param onBoardingBottomArea OnBoarding 각 스탭 화면에 관계 없이 유지할 하단 영역 컴포저블. 없으면 출력 하지 않는다.
+ */
 @Composable
 fun <CustomStep : OnBoardingRoute> OnBoardingScreen(
     steps: List<CustomStep>,
-    currentStep: Int = 0,
+    startStep: Int = 0,
     onBoardingTopArea: (@Composable (NavHostController, OnBoardingViewModel) -> Unit)? = null,
     onBoardingBottomArea: (@Composable (NavHostController, OnBoardingViewModel) -> Unit)? = null,
 ) {
@@ -20,7 +28,7 @@ fun <CustomStep : OnBoardingRoute> OnBoardingScreen(
         OnBoardingViewModelImpl(
             navController = onBoardingNavController,
             steps = steps,
-            currentStep = currentStep,
+            startStep = startStep,
         )
     }
     Column {
@@ -34,6 +42,12 @@ fun <CustomStep : OnBoardingRoute> OnBoardingScreen(
     }
 }
 
+/**
+ * 스탭 이동 시 내비게이션 처리.
+ * @param onBoardingNavController OnBoarding 내비게이션에 사용되는 [NavHostController]
+ * @param onBoardingViewModel OnBoarding 진행에 사용.
+ * @param steps 사용자가 입력한 OnBoarding 스탭들.
+ */
 @Composable
 private fun <CustomStep : OnBoardingRoute> OnBoardingNavHost(
     onBoardingNavController: NavHostController,
