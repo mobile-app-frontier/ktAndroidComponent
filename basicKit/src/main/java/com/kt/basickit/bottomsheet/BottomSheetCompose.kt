341f1647bd2655a1a1192b5fa53ccb48ac41b762
khjang <jang.kyounghee@kt.com>
2023-09-04 08:54:36 +0900

    [fix] bottomSheet 관련 파일 분리

--- basicKit/src/main/java/com/kt/basickit/bottomsheet/BottomSheetCompose.kt
+++ basicKit/src/main/java/com/kt/basickit/bottomsheet/BottomSheetCompose.kt
@@ -7,11 +7,8 @@ import androidx.compose.animation.slideInVertically
 import androidx.compose.animation.slideOutVertically
 import androidx.compose.foundation.ScrollState
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -27,8 +24,6 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.composed
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Popup
@@ -48,60 +43,49 @@ fun BottomSheetCompose(
     content: @Composable () -> Unit,
 ) {
     val density = LocalDensity.current
-    val contentsModifier = if (options.contentHeight != null) {
-        Modifier
-            .background(
-                options.contentBackgroundColor,
-                shape = RoundedCornerShape(
-                    options.borderRadius,
-                    options.borderRadius,
-                    0,
-                    0,
-                ),
-            )
-            .padding(options.contentPadding)
-            .fillMaxWidth(options.contentWidth)
-            .height(options.contentHeight.dp)
-            .verticalScroll(scrollState)
-            .noRippleClickable {}
-            .zIndex(1.0f)
-    } else {
-        Modifier
-            .background(
-                options.contentBackgroundColor,
-                shape = RoundedCornerShape(
-                    options.borderRadius,
-                    options.borderRadius,
-                    0,
-                    0,
-                ),
-            )
-            .padding(options.contentPadding)
-            .fillMaxWidth(options.contentWidth)
-            .verticalScroll(scrollState)
-            .noRippleClickable {}
-            .zIndex(1.0f)
-    }
+
+    // 켜고 끌때 Transition Animation
     val animationState = remember {
         MutableTransitionState(false).apply {
             targetState = true
         }
     }
-    var offsetY by remember { mutableFloatStateOf(options.contentHeight ?: density.density) }
+
     if (!animationState.targetState && !animationState.currentState) {
         onDismissRequest()
     }
 
+    var offsetY by remember { mutableFloatStateOf(options.contentHeight ?: density.density) }
     fun animateBeforeClose() {
         animationState.targetState = false
     }
 
     fun onChangeOffset(delta: Float) {
-        offsetY -= (delta / 3) // 감도 조절..
+        offsetY += (delta / 3) // 감도 조절..
 
         if (offsetY < 50f) {
             animateBeforeClose()
         }
+    }
+
+    var contentsModifier = Modifier
+        .background(
+            options.contentBackgroundColor,
+            shape = RoundedCornerShape(
+                options.borderRadius,
+                options.borderRadius,
+                0,
+                0,
+            ),
+        )
+        .padding(options.contentPadding)
+        .fillMaxWidth(options.contentWidth)
+        .verticalScroll(scrollState)
+        .noRippleClickable {}
+        .zIndex(1.0f)
+
+    if (options.contentHeight != null) {
+        contentsModifier = contentsModifier.height(options.contentHeight.dp)
     }
 
     Popup(
@@ -145,34 +129,5 @@ fun BottomSheetCompose(
                 }
             }
         }
-    }
-}
-
-/**
- * Bottom sheet options
- *
- * @property dimmingColor: BottomSheet 배경 Dimming 색깔
- * @property borderRadius: BottomSheet 좌, 우측 상단 BorderRadius 값
- * @property contentBackgroundColor
- * @property contentPadding: 내부 Content 의 패딩
- * @property contentWidth: 가로 크기의 비율. ex) 0.7f 일 경우 가로의 70%크기의 BottomSheet 가 만들어 짐.
- * @constructor 모든 요소는 기본 값이 있어서 따로 지정 하지 않으면 기본 BottomSheet 디자인이 적용됨.
- */
-data class BottomSheetOptions(
-    val isDraggable: Boolean = true,
-    val dimmingColor: Color = Color.Gray.copy(alpha = 0.2f),
-    val borderRadius: Int = 10,
-    val contentBackgroundColor: Color = Color.White,
-    val contentPadding: PaddingValues = PaddingValues(20.dp, 10.dp),
-    val contentWidth: Float = 1f, // 쓸까..?
-    val contentHeight: Float? = null,
-)
-
-inline fun Modifier.noRippleClickable(crossinline onClick: () -> Unit): Modifier = composed {
-    clickable(
-        indication = null,
-        interactionSource = remember { MutableInteractionSource() },
-    ) {
-        onClick()
     }
 }
---
+++ basicKit/src/main/java/com/kt/basickit/bottomsheet/BottomSheetOptions.kt
@@ -0,0 +1,25 @@
+package com.kt.basickit.bottomsheet
+
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+
+/**
+ * Bottom sheet options
+ *
+ * @property dimmingColor: BottomSheet 배경 Dimming 색깔
+ * @property borderRadius: BottomSheet 좌, 우측 상단 BorderRadius 값
+ * @property contentBackgroundColor
+ * @property contentPadding: 내부 Content 의 패딩
+ * @property contentWidth: 가로 크기의 비율. ex) 0.7f 일 경우 가로의 70%크기의 BottomSheet 가 만들어 짐.
+ * @constructor 모든 요소는 기본 값이 있어서 따로 지정 하지 않으면 기본 BottomSheet 디자인이 적용됨.
+ */
+data class BottomSheetOptions(
+    val isDraggable: Boolean = true,
+    val dimmingColor: Color = Color.Gray.copy(alpha = 0.2f),
+    val borderRadius: Int = 10,
+    val contentBackgroundColor: Color = Color.White,
+    val contentPadding: PaddingValues = PaddingValues(20.dp, 10.dp),
+    val contentWidth: Float = 1f, // 쓸까..?
+    val contentHeight: Float? = null,
+)
